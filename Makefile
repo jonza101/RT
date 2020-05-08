de1965eed43e5693b1abc996e04e17bbb39a36c9
wstygg <wstygg@student.21-school.ru>
2020-05-08 21:04:16 +0800

    better makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,100 @@
-SRC = main.c render.c funcs.c intersect.c normal.c camera.c keys.c gpu.c init.c texture.c
-SRC += gnl/get_next_line.c gnl/libft/libft.a
-INC = -I gnl/ -I /usr/X11/include
+NAME		=	RT
 
-FLAGS = -g -Ofast
+CC			=	gcc
+FLAGS		=	-Wall -Wextra -Werror -g3 -Ofast
 
-# #all:
-# 	#gcc $(FLAGS) $(SRC) $(INC) -lm -lpthread -lOpenCL -lmlx -lXext -lX11 -I minilibx/ -L minilibx/
+DELTA		=	$$(echo "$$(tput cols)-47"|bc)
 
- all:
-	gcc $(FLAGS) $(SRC) $(INC) -lm -lpthread -lOpenCL -L/usr/X11/lib /usr/X11/lib/libmlx.a -lXext -lX11 -lbsd
+LIBFT_DIR	=	libft/
+LIBFT_LIB	=	$(LIBFT_DIR)libft.a
+LIBFT_INC	=	$(LIBFT_DIR)/inc/
+
+LIBS		=	-lm -lpthread -lOpenCL -L/usr/X11/lib /usr/X11/lib/libmlx.a -lXext -lX11 -lbsd
+
+SRC_DIR		=	Sources/
+INC_DIR		=	Includes/
+OBJ_DIR		=	.obj/
+
+SRC_BASE	=	main.c		\
+				render.c	\
+				funcs.c		\
+				intersect.c	\
+				normal.c	\
+				camera.c	\
+				keys.c		\
+				gpu.c		\
+				init.c		\
+				texture.c	\
+
+SRCS		=	$(addprefix $(SRC_DIR), $(SRC_BASE))
+OBJS		=	$(addprefix $(OBJ_DIR), $(SRC_BASE:.c=.o))
+NB			=	$(words $(SRC_BASE))
+INDEX		=	0
+
+all :
+	@make -C $(LIBFT_DIR)
+	@make -j $(NAME)
+
+$(NAME):		$(LIBFT_LIB) $(OBJ_DIR) $(OBJS)
+	@$(CC) $(OBJS) -o $(NAME) \
+		-I $(INC_DIR) \
+		-I $(LIBFT_INC) \
+		$(LIBS) \
+		$(LIBFT_LIB) \
+		$(FLAGS)
+	@printf "\r\033[48;5;15;38;5;25m✅ MAKE $(NAME)\033[0m\033[K\n"
+
+$(LIBFT_LIB):
+	@make -C $(LIBFT_DIR)
+
+$(OBJ_DIR) :
+	@mkdir -p $(OBJ_DIR)
+	@mkdir -p $(dir $(OBJS))
+
+$(OBJ_DIR)%.o :	$(SRC_DIR)%.c | $(OBJ_DIR)
+	@$(eval DONE=$(shell echo $$(($(INDEX)*20/$(NB)))))
+	@$(eval PERCENT=$(shell echo $$(($(INDEX)*100/$(NB)))))
+	@$(eval COLOR=$(shell echo $$(($(PERCENT)%35+196))))
+	@$(eval TO_DO=$(shell echo $$((20-$(INDEX)*20/$(NB)))))
+	@printf "\r\033[38;5;11m⌛ MAKE %10.10s : %2d%% \033[48;5;%dm%*s\033[0m%*s\033[48;5;255m \033[0m \033[38;5;11m %*.*s\033[0m\033[K" $(NAME) $(PERCENT) $(COLOR) $(DONE) "" $(TO_DO) "" $(DELTA) $(DELTA) "$@"
+	@$(CC)  -MMD -c $< -o $@\
+		-I $(INC_DIR)\
+		-I $(LIBFT_INC)
+	@$(eval INDEX=$(shell echo $$(($(INDEX)+1))))
+
+clean:          cleanlib
+	@rm -rf $(OBJ_DIR)
+	@printf "\r\033[38;5;202m✖ clean $(NAME)\033[0m\033[K\n"
+
+cleanlib:
+	@make -C $(LIBFT_DIR) clean
+
+cleansh:
+	@rm -rf $(OBJ_DIR)
+	@printf "\r\033[38;5;202m✖ clean $(NAME)\033[0m\033[K\n"
+
+fclean:			fcleanlib cleansh
+	@rm -f $(NAME)
+	@printf "\r\033[38;5;196m❌ fclean $(NAME)\033[0m\033[K\n"
+
+fcleanlib:
+	@make -C $(LIBFT_DIR) fclean
+
+re:				fclean all
+
+.PHONY :		fclean clean re cleanlib fcleanlib
+
+-include $(OBJS:.o=.d)
+
+
+#SRC = main.c render.c funcs.c intersect.c normal.c camera.c keys.c gpu.c init.c texture.c
+#SRC += libft/libft.a
+#INC = -I libft/inc
+#
+#FLAGS = -g -Ofast
+#
+## #all:
+## 	#gcc $(FLAGS) $(SRC) $(INC) -lm -lpthread -lOpenCL -lmlx -lXext -lX11 -I minilibx/ -L minilibx/
+#
+# all:
+#	gcc $(FLAGS) $(SRC) $(INC) -lm -lpthread -lOpenCL -L/usr/X11/lib /usr/X11/lib/libmlx.a -lXext -lX11 -lbsd
---
+++ Sources/init.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zjeyne-l <zjeyne-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/30 20:15:28 by zjeyne-l          #+#    #+#             */
+/*   Updated: 2020/02/21 01:40:33 by zjeyne-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+
+void	ft_strsplit_free(char **split)
+{
+	int i = -1;
+	while (split[++i])
+		free(split[i]);
+	free(split);
+}
+
+void	ft_init_txt(t_mlx *mlx)
+{
+	int		fd;
+	char	*line;
+
+	char *txt[TXT] = {"textures/wbg_tile.xpm", "textures/fabric.xpm", "textures/g_tile.xpm",
+						"textures/paint.xpm"};
+
+	int i = -1;
+	while (++i < TXT)
+	{
+		fd = open(txt[i], O_RDONLY);
+		int j = -1;
+		while (++j < 4)
+		{
+			get_next_line(fd, &line);
+			(j < 3) ? ft_strdel(&line) : 1;
+		}
+		char **tmp = ft_strsplit(line, " ");
+		mlx->txt[i] = (t_img*)malloc(sizeof(t_img));
+		mlx->txt[i]->w = atoi(&tmp[0][1]);
+		mlx->txt[i]->h = atoi(tmp[1]);
+		mlx->txt[i]->txt_idx = i;
+		mlx->txt[i]->img = mlx_xpm_file_to_image(mlx->mlx, txt[i], &mlx->txt[i]->w, &mlx->txt[i]->h);
+		mlx->txt[i]->data = (int*)mlx_get_data_addr(mlx->txt[i]->img, &mlx->txt[i]->bpp, &mlx->txt[i]->size_line, &mlx->txt[i]->endian);
+		ft_strsplit_free(tmp);
+		ft_strdel(&line);
+		close(fd);
+	}
+}
+
+void	ft_init_norm(t_mlx *mlx)
+{
+	int 	fd;
+	char 	*line;
+
+	char	*norm[NORM] = {"textures/norm/wbg_tile_norm.xpm", "textures/norm/fabric_norm.xpm",
+							"textures/norm/g_tile_norm.xpm", "textures/norm/paint_norm.xpm"};
+
+	int i = -1;
+	while (++i < NORM)
+	{
+		fd = open(norm[i], O_RDONLY);
+		int j = -1;
+		while (++j < 4)
+		{
+			get_next_line(fd, &line);
+			(j < 3) ? ft_strdel(&line) : 1;
+		}
+		char **tmp = ft_strsplit(line, " ");
+		mlx->norm[i] = (t_img *)malloc(sizeof(t_img));
+		mlx->norm[i]->w = atoi(&tmp[0][1]);
+		mlx->norm[i]->h = atoi(tmp[1]);
+		mlx->norm[i]->txt_idx = i;
+		mlx->norm[i]->img = mlx_xpm_file_to_image(mlx->mlx, norm[i], &mlx->norm[i]->w, &mlx->norm[i]->h);
+		mlx->norm[i]->data = (int *)mlx_get_data_addr(mlx->norm[i]->img, &mlx->norm[i]->bpp, &mlx->norm[i]->size_line, &mlx->norm[i]->endian);
+		ft_strsplit_free(tmp);
+		ft_strdel(&line);
+		close(fd);
+	}
+}
+
+void	ft_init_rgh(t_mlx *mlx)
+{
+	int		fd;
+	char	*line;
+
+	char	*rgh[RGH] = {"textures/rgh/wbg_tile_rgh.xpm", "textures/rgh/fabric_rgh.xpm",
+						"textures/rgh/g_tile_rgh.xpm", "textures/rgh/paint_rgh.xpm"};
+
+	int i = -1;
+	while (++i < RGH)
+	{
+		fd = open(rgh[i], O_RDONLY);
+		int j = -1;
+		while (++j < 4)
+		{
+			get_next_line(fd, &line);
+			(j < 3) ? ft_strdel(&line) : 1;
+		}
+		char **tmp = ft_strsplit(line, " ");
+		mlx->rgh[i] = (t_img*)malloc(sizeof(t_img));
+		mlx->rgh[i]->w = atoi(&tmp[0][1]);
+		mlx->rgh[i]->h = atoi(tmp[1]);
+		mlx->rgh[i]->txt_idx = i;
+		mlx->rgh[i]->img = mlx_xpm_file_to_image(mlx->mlx, rgh[i], &mlx->rgh[i]->w, &mlx->rgh[i]->h);
+		mlx->rgh[i]->data = (int *)mlx_get_data_addr(mlx->rgh[i]->img, &mlx->rgh[i]->bpp, &mlx->rgh[i]->size_line, &mlx->rgh[i]->endian);
+		ft_strsplit_free(tmp);
+		ft_strdel(&line);
+		close(fd);
+	}
+	
+}
